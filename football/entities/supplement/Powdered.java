e01759789bee70497cd9dcdad0822263f201fae0
Dragomir Petrov <103362620+DPPetrov@users.noreply.github.com>
2022-12-08 16:14:48 +0200

    Add files via upload

---
+++ football/entities/supplement/Powdered.java
@@ -0,0 +1,21 @@
+package football.entities.supplement;
+
+public class Powdered extends BaseSupplement {
+
+    private static final int ENERGY = 120;
+    private static final double PRICE = 15;
+
+    public Powdered() {
+        super(ENERGY, PRICE);
+    }
+
+    @Override
+    public int getEnergy() {
+        return 0;
+    }
+
+    @Override
+    public double getPrice() {
+        return 0;
+    }
+}
